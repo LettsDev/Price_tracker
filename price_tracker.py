a856d9da27e0b250a5ff1e3e66618eb7d327f41e
Jared <68018136+mystic-cat@users.noreply.github.com>
2020-11-12 11:37:00 -0800

    Coded
    Finished command_logic and started add new entry function

--- price_tracker.py
+++ price_tracker.py
@@ -4,21 +4,40 @@ import os.path
 from os import path
 import re
 
-# class User (self, email_address):
-#     def __init__(self):
-#         pass
-#     def _add(self):
-#         pass
-#     def _purge(self):
-#         pass
-#     def _help(self):
-#         pass
-#     def _status(self):
-#         pass
-#     def _admin(self):
-#         pass
-#     def _delete(self):
-#         pass
+user_data = {}  #item number, name, email address, item URL, description
+price_data = {} #line number, item number, date, price
+welcome_message = ""  #TODO create generic welcome message
+help_message = ""   #TODO create help message 
+
+class User (self, message):
+    def __init__(self):
+        self.message = message  #message ->  [sender name, email address, command, [list of URLs]]
+    
+    def _add(self):
+        items = []
+        if self.message[1] not in user_data.values():
+            welcome_email(self.message[0],self.message[1])
+        for URL in self.message[3]:
+            for information in user_data.values():
+                if  self.message[1] and URL in information:
+                    ezgmail.send(self.message[1], "Already exists", f"You already have {information[4]} added to your watch list.")
+                    return None
+        
+        for item_num in user_data.keys():   #adds item to user_data
+            items.append(item_num)
+        items.sort()
+        user_data[items[len(items) + 1]] = [self.message[0],self.message[1],self.message[2],self.message[3]]
+        #TODO Save User data to CSV
+    def _purge(self):
+        pass
+    def _help(self):
+        pass
+    def _status(self):
+        pass
+    def _admin(self):
+        pass
+    def _delete(self):
+        pass
 
 # class Scrape(self, item_url):
 #     def __init__(self):
@@ -32,12 +51,12 @@ import re
 #     def _delete(self):
 #         pass
 
-def user_csv_exist():    #Is there a csv file 
+def user_csv_exist():    
     if path.exists("Users_and_items.csv"):
         pass
     else:
         with open("Users_and_items.csv","w") as file:
-            headers = ["Item Number","Email Addresses", "Item URL", "Description"]
+            headers = ["Item Number","Name","Email Address", "Item URL", "Description"]
             csv_writer = DictWriter(file, fieldnames = headers)
             csv_writer.writeheader()
 
@@ -46,31 +65,31 @@ def price_csv_exist():
         pass
     else:
         with open("Prices.csv", "w") as file:
-            price_headers = ["Line number","Item number", "Date", "Price"]
+            price_headers = ["Entry number","Item number", "Date", "Price"]
             csv_writer = DictWriter(file, fieldnames = price_headers)
             csv_writer.writeheader()
     
 def load_user_data():
     user_data = {}
-    with open("Users_and_items.csv") as file:
+    with open("Users_and_items.csv","r") as file:
         csv_reader = DictReader(file)
         for row in csv_reader:
-            user_data[row["Item Number"]] = [row["Email Addresses"], row["Item URL"], row["Description"]]
+            user_data[row["Item Number"]] = [row["Name"], row["Email Address"], row["Item URL"], row["Description"]]
         return user_data
 
 def load_price_data():
     prices = {}
-    with open("Prices.csv") as file:
+    with open("Prices.csv", "r") as file:
         csv_reader = DictReader(file)
         for row in csv_reader:
-            prices[row["Line number"]] = [row["Item number"], row["Date"], row["Price"]]
+            prices[row["Entry number"]] = [row["Item number"], row["Date"], row["Price"]]
         return prices
 
 def email_list(data):
-    unique_addresses = []
+    unique_addresses = []   #Gets email addresses from CSV
     for info in data.values():
         if info[0] not in unique_addresses:
-            unique_addresses.append(info[0])
+            unique_addresses.append(info[1])
     return unique_addresses
 
 def check_email_inbox():
@@ -96,31 +115,71 @@ def get_new_mail(new_mail):
     """, re.VERBOSE)
     
     for i in range(num_thread):
-            message_info[i] = [(name_pattern.search(new_mail[i].messages[0].sender)).group(0).strip(),
-            (email_pattern.search(new_mail[i].messages[0].sender)).group(),
+            message_info[i] = [(name_pattern.search(new_mail[i].messages[0].sender)).group(0).strip(),  #Extract name 
+            (email_pattern.search(new_mail[i].messages[0].sender)).group(), #Extract email
             new_mail[i].messages[0].subject,
             new_mail[i].messages[0].body]
     return message_info
 
+def welcome_email(name, email):
+    ezgmail.send(email,f"Hey! Welcome {name}!",
+    f"Hi {name}!\n{welcome_message}\n{help_message}")
 def confirmation_email(email_address):
     ezgmail.send(email_address, 'Received your email',"Confirmed")  #TODO Add in relelvent information to the confirmation email body
 
-def validation(messages):   #If valid information was sent in an email
-    for message in messages.values():
-        message[1]
-    pass
+def command_logic(messages):   #When a user sends a command 
+
+#**********************************Email Error Checking**********************************
+    commands = ("help", "status", "delete", "purge", "new user", "add")
+    website_pattern = re.compile(r"(https?:\/\/(?:www\.|(?!www))[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\.[^\s]{2,}|www\.[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\.[^\s]{2,}|https?:\/\/(?:www\.|(?!www))[a-zA-Z0-9]+\.[^\s]{2,}|www\.[a-zA-Z0-9]+\.[^\s]{2,})"
+    ,re.IGNORECASE)
+    
+    for message in messages.values():   #message -> [sender name, email address, subject, body]
+        
+        result = website_pattern.search(message[3]) #looking for URls in the body of the message
+        message[2] = message[2].lower().strip() #formatting the subject
+        message[3] = re.findall(result, message[3]) #[formatting the URLs]
+        
+        if message[2] not in commands:  #Was the correct command used in the subject line?
+            ezgmail.send(message[1], "There was a problem with your request!",
+             f"The command used in your last email: ({message[2]}) was mot understood.\n Please use one of the following commands in the subject line of your email: {commands}")   #TODO Replace with help message
+            return None #For Error
+        
+        elif result == None:    #Are the URLs valid?
+            ezgmail.send(message[1], "There was a problem with your request!",
+            f"Valid URL(s) could not be found in: {message[3]}\nPlease try a different URL!")
+            return None #For error
+ 
+ #TODO add checking for the websites that are able to be scraped
+
+#**********************************Command Sort**********************************
+        else:  
+            mailer = User(message)  #Command logic
+            if message[2] == "add":
+                mailer._add()
+            elif message[2] == "help":
+                mailer._help()
+            elif message[2] == "status":
+                mailer._status()
+            elif message[2] == "delete":
+                mailer._delete()
+            elif message[2] == "purge":
+                mailer._purge()
+    return True
 
 def main():
-    user_csv_exist()
+    user_csv_exist()    #Initialization to make sure there are CSV files
     price_csv_exist()
-    user_data = load_user_data()
-    price_data = load_price_data()
-    current_email_addresses = email_list(user_data)
-    
-    new_mail = check_email_inbox()
+
+    new_mail = check_email_inbox()  #Checking for new mail
     if new_mail:
         new_messages = get_new_mail(new_mail)   #Dict format->  Email num: [sender name, email address, subject, body]
-    
-    print(new_messages)
+
+    if command_logic(new_messages):
+
+        user_data = load_user_data()    
+        price_data = load_price_data()
+        current_email_addresses = email_list(user_data)
+        print(new_messages)
 main()
 
